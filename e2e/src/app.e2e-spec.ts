bd1a095f75e61065e930ff86490c593b5a0667ba
Benjamin Liden <benjamin.c.liden@gmail.com>
2021-09-25 12:38:59 -0400

    build: fix name to match new repo title

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to digital-witchcraft!');
+    expect(page.getTitleText()).toEqual('Welcome to digital-witch-project!');
   });
 
   afterEach(async () => {
